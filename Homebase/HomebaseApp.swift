a0328891e35c4bfa88e4adc1ceab36cefa142a64
Kristoffer <kristoffer@bonsy.no>
2026-01-18 21:40:24 +0100

    Initial Commit

---
+++ Homebase/HomebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomebaseApp.swift
+//  Homebase
+//
+//  Created by Kristoffer Follestad on 18/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct HomebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
